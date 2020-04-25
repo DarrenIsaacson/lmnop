0890a046a50b1aef2322a6ba6d57e7f312b93032
Flounder <testy@test.com>
2020-04-25 00:48:40 -0500

    Refined database, created base test functions

--- lmn/tests/test_models.py
+++ lmn/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UProfile
 from django.db import IntegrityError
 # Create your tests here.
 
@@ -44,7 +44,56 @@ class TestUser(TestCase):
         with self.assertRaises(IntegrityError):
             user2.save()
 
+<<<<<<< HEAD
 
 
 
 
+=======
+#Test User Profile, create, update, save picture, delete picture
+class TestUserProfile(TestCase):
+    
+    def test_create_user_profile(self):
+        user = User(username='littleFoot', email='lilfoo@b4time.com', first_name='Little', last_name='Foot')
+        user.save()
+
+        uProfile = UProfile(user=0, birthday='', city='Minneapolis', state='MN', favoriteVenue='First Ave.', favoriteArtist='Kind Country', description='I love local music. Everything from Eydea & Abilities to Trampled by Turtles')
+        uProfile.save()
+        #check to make sure UProfile was created
+
+    def test_update_user_profile(self):
+        user = User(username='littleFoot', email='lilfoo@b4time.com', first_name='Little', last_name='Foot')
+        user.save()
+
+        uProfile = UProfile(user=0, birthday='', city='Minneapolis', state='MN', favoriteVenue='First Ave.', favoriteArtist='Kind Country', description='I love local music. Everything from Eydea & Abilities to Trampled by Turtles')
+        uProfile.save()
+        #check that user profile was updated
+
+
+    def test_save_user_profile_picture(self):
+        user = User(username='littleFoot', email='lilfoo@b4time.com', first_name='Little', last_name='Foot')
+        user.save()
+
+        uProfile = UProfile(user=0, birthday='', city='Minneapolis', state='MN', favoriteVenue='First Ave.', favoriteArtist='Kind Country', description='I love local music. Everything from Eydea & Abilities to Trampled by Turtles')
+        uProfile.save()
+
+        #add/save image and check that it is saved
+
+    def test_delete_user_profile_picture(self):
+        user = User(username='littleFoot', email='lilfoo@b4time.com', first_name='Little', last_name='Foot')
+        user.save()
+        #add image to profile creation
+        uProfile = UProfile(user=0, birthday='', city='Minneapolis', state='MN', favoriteVenue='First Ave.', favoriteArtist='Kind Country', description='I love local music. Everything from Eydea & Abilities to Trampled by Turtles')
+        uProfile.save()
+        #test to make sure profile image is there
+        #delete procifile picture and chech that it was deleted
+
+    def test_change_user_profile_picture(self):
+        user = User(username='littleFoot', email='lilfoo@b4time.com', first_name='Little', last_name='Foot')
+        user.save()
+        #add image to profile creation
+        uProfile = UProfile(user=0, birthday='', city='Minneapolis', state='MN', favoriteVenue='First Ave.', favoriteArtist='Kind Country', description='I love local music. Everything from Eydea & Abilities to Trampled by Turtles')
+        uProfile.save()
+        #test to make sure profile image changed
+        
+>>>>>>> 44515f4... Refined database, created base test functions
