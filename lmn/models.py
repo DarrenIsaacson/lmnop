a24257d6a28e9f45da55a08841aa01386b955289
Flounder <testy@test.com>
2020-04-27 22:30:01 -0500

    Profile table test updates

--- lmn/models.py
+++ lmn/models.py
@@ -37,6 +37,10 @@ class Venue(models.Model):
 ''' A show - one artist playing at one venue at a particular date. '''
 class Show(models.Model):
     
+<<<<<<< HEAD
+=======
+    show_date = models.DateTimeField(blank=False)
+>>>>>>> 88bc6fd... Profile table test updates
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, blank=False)
