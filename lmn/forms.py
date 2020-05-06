76e865e6ae2c9e17802d994d4fae3ff20fc72d45
Flounder <testy@test.com>
2020-05-05 21:11:52 -0500

    commit b4 merge from clara

--- lmn/forms.py
+++ lmn/forms.py
@@ -86,7 +86,7 @@ class UserProfileForm(forms.ModelForm):
         model = UProfile
         fields = ('birthday', 'city', 'state', 'favoriteVenue', 'favoriteArtist', 'profilePicture', 'description')
 
-    def save(self, commit=True):
+    """def save(self, commit=True):
         UProfile = super(UserProfileForm, self).save(commit=False)
         UProfile.birthday = self['birthday']
         uProfile.city = self['city']
@@ -96,7 +96,4 @@ class UserProfileForm(forms.ModelForm):
         uProfile.profilePicture = self['profilePicture']
         uProfile.description = self['description']
 
-        if commit:
-            uProfile.save()
-
-        return uProfile   
+        return UProfile   """
