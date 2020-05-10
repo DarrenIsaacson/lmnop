ef036670f329bf93740a76595550d72772a75e7c
Ben Wadsworth <btwadsworth@gmail.com>
2020-05-10 15:04:47 -0500

    Added first test

--- lmn/tests/test_notes.py
+++ lmn/tests/test_notes.py
@@ -4,7 +4,14 @@ from django.urls import reverse
 from django.contrib import auth
 
 from lmn.models import Venue, Artist, Note, Show
-from django.contrib.auth.models import User, Note, Venue, Artist, 
+from django.contrib.auth.models import User 
 
 import re, datetime
 from datetime import timezone
+
+
+class TestEmpyNotes(TestCase):
+    
+    def test_notes_page_with_no_notes(self):
+        response = self.client.get(reverse('lmn:latest_notes'))
+        self.assertFalse(response.context['notes'])
