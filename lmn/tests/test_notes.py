aa457d592b3d46c44bfd4573c8c57c22aaaf6764
Ben Wadsworth <btwadsworth@gmail.com>
2020-05-10 15:38:21 -0500

    Added a few tests and reajusted html files.

--- lmn/tests/test_notes.py
+++ lmn/tests/test_notes.py
@@ -11,7 +11,21 @@ from datetime import timezone
 
 
 class TestEmpyNotes(TestCase):
-    
+
+    # Test having no notes returns an empty list
     def test_notes_page_with_no_notes(self):
         response = self.client.get(reverse('lmn:latest_notes'))
-        self.assertFalse(response.context['notes'])
+        self.assertFalse(response.context['notes'])  # empty list is false
+
+    def test_top_shows_page_with_no_notes(self):
+        response = self.client.get(reverse('lmn:top_shows'))
+        self.assertFalse(response.context['shows'])  # empty dictionary is false
+
+    def test_notes_page_text_with_no_notes(self):
+        response = self.client.get(reverse('lmn:latest_notes'))
+        self.assertContains(response, 'No notes.')  # Check if "No notes." is on page text
+
+    def test_top_shows_page_text_with_no_notes(self):
+        response = self.client.get(reverse('lmn:top_shows'))
+        self.assertContains(response, 'There are no notes yet check back later or add your own!')  # Check page text contains statement
+    
