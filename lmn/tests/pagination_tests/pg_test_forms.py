3b37ca962f8d9cbf7fb002d0008a7415c472d223
Darren Isaacson <yc8121yl@go.minnstate.edu>
2020-05-12 03:07:33 -0500

    rough draft of one test

--- lmn/tests/pagination_tests/pg_test_forms.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-from django.urls import reverse, resolve
-
-from lmn.views_artists import artist_list
-from lmn.views_notes import latest_notes
-from lmn.views_users import user_profile
-from lmn.views_venues import venue_list
-
