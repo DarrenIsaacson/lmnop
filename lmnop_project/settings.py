d69907889aba027cb49503a8ff5afebff9b84273
B33 <my@email.com>
2020-05-13 11:30:57 -0500

    Turn debug mode off on depoyed version

--- lmnop_project/settings.py
+++ lmnop_project/settings.py
@@ -23,8 +23,11 @@ SECRET_KEY = '8c01$#j44g3znb)$q0()8)!%ts-jc)k12!a75-!63qb%bj=d4k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
+if os.getenv('GAE_INSTANCE'):
+    DEBUG = False
+else:
+    DEBUG = True
 
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
